643427b2e32a9ec8e817de7f5457df7cfff57b9a
= <=>
2019-10-06 14:23:35 +0100

    navigation bar for users

---
+++ components/navbar.php
@@ -0,0 +1,48 @@
+<section id="screen1">
+
+
+
+	<!-- <nav>
+		 <ul>
+   <li><a href="./index.php">Home</a></li>
+		 	  <li><a href="houses.php">Available houses</a></li>
+		 	  <li><a href="roommates.php">Available Roommates</a></li>
+		 	  <li><a href="login.php"><a href="#" data-toggle="modal" data-target="#modalLRForm">Login / Register</a></a></li>
+		 </ul>
+	</nav> -->
+
+
+	<!--Navbar -->
+	<nav class="mb-1 navbar navbar-expand-lg navbar-dark">
+	  <a class="navbar-brand" href="index.php">Fuostay</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4"
+	    aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item ">
+	        <a class="nav-link" href="houses.php">
+	          Find house
+
+	        </a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="roommates.php">
+	          Find roommate</a>
+	      </li>
+				<li class="nav-item">
+	        <a class="nav-link" href="login.php">
+	          Log in</a>
+	      </li>
+				<li class="nav-item">
+	        <a class="nav-link" href="signup.php">
+	          Sign up</a>
+	      </li>
+
+	    </ul>
+	  </div>
+	</nav>
+	<!--/.Navbar -->
+
+</section>
