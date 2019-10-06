5a07bc8e8966a72814490a7ea15a083384847764
= <=>
2019-10-06 14:11:27 +0100

    sidebar component for admin dashboard

---
+++ admin/components/sidebar.php
@@ -0,0 +1,38 @@
+<!-- Sidebar -->
+<div class=" border-right" id="sidebar-wrapper">
+  <div class="sidebar-heading">Fuostay Admin </div>
+  <div class="list-group list-group-flush">
+    <a href="dashboard.php" class="list-group-item list-group-item-action ">Dashboard</a>
+    <a href="approvelisting.php" class="list-group-item list-group-item-action ">Approve listing</a>
+    <a href="users.php" class="list-group-item list-group-item-action ">View all users</a>
+    <a href="sendmail.php" class="list-group-item list-group-item-action ">Send emails</a>
+    <a href="admins.php" class="list-group-item list-group-item-action ">Manage admins</a>
+    <a href="logout.php" class="list-group-item list-group-item-action ">Logout</a>
+  </div>
+</div>
+<!-- /#sidebar-wrapper -->
+<!-- Page Content -->
+<div id="page-content-wrapper">
+<nav class="navbar navbar-expand-lg ">
+  <span id="menu-toggle"><i class="fas fa-bars"></i></span>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span>Fuostay Admin</span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+      <!-- <li class="nav-item active">
+        <span class="nav-link">Welcome Samuel </span>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="messages.php">Messages <span style=" display: inline-block; background: black; width: 27px; border-radius: 50%;">2</span></a>
+      </li> -->
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Log out</a>
+      </li>
+
+
+    </ul>
+  </div>
+</nav>
