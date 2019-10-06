5ea5b89f90bfc0f40ad8376c10dd61abb20b0edf
= <=>
2019-10-06 14:28:12 +0100

    script to delete listing by users

---
+++ db/delete_listing.php
@@ -0,0 +1,12 @@
+<?php
+require_once('startsession.php');
+require_once('../db/connect.php');
+
+
+$id = $_POST['user_id'];
+
+$sql = "DELETE FROM listings WHERE id = '$id'";
+mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+
+?>
